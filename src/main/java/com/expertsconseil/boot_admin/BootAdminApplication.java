010ffc33ed88920fa6ab4f435a145bcae98ff14e
maher.formations@gmail.com <maher.formations@gmail.com>
2025-09-20 03:08:08 +0200

    Update loadbalancing config

--- src/main/java/com/expertsconseil/boot_admin/BootAdminApplication.java
+++ src/main/java/com/expertsconseil/boot_admin/BootAdminApplication.java
@@ -2,9 +2,13 @@ package com.expertsconseil.boot_admin;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableAdminServer
+@EnableAutoConfiguration
+@EnableDiscoveryClient
 @SpringBootApplication
 public class BootAdminApplication {
 
